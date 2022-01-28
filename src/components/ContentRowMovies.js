1d5c49b4b9c60b46aa7f305a1e6a48b36f2ff66e
Gscalmati <gioscalmati@hotmail.com>
2022-01-27 21:26:18 -0300

    Modifico carteles de dashboard con Users Cuantity, Products Cuantity y Categories Cuantity

--- src/components/ContentRowMovies.js
+++ src/components/ContentRowMovies.js
@@ -1,38 +1,79 @@
 import React from 'react';
 import SmallCard from './SmallCard';
+//import fetch from 'node-fetch';
+import {useEffect, useState} from 'react';
 
 /*  Cada set de datos es un objeto literal */
 
 /* <!-- Movies in DB --> */
 
-let moviesInDB = {
-    title: 'Movies in Data Base',
+let apiUsers = {};
+let apiProducts = {};
+
+   
+    
+    let usersInDb = {
+    title: 'Users in DataBase',
     color: 'primary', 
-    cuantity: 21,
+    cuantity:'',
     icon: 'fa-clipboard-list'
 }
 
 /* <!-- Total awards --> */
 
-let totalAwards = {
-    title:' Total awards', 
+let productsInDb = {
+    title:'Products in DataBase', 
     color:'success', 
-    cuantity: '79',
+    cuantity: '',
     icon:'fa-award'
 }
 
 /* <!-- Actors quantity --> */
 
-let actorsQuantity = {
-    title:'Actors quantity' ,
+let categoriesInDb = {
+    title:'Categories in DataBase' ,
     color:'warning',
-    cuantity:'49',
+    cuantity:'',
     icon:'fa-user-check'
 }
 
-let cartProps = [moviesInDB, totalAwards, actorsQuantity];
+
+let cartProps = [usersInDb, productsInDb, categoriesInDb];
 
 function ContentRowMovies(){
+
+
+
+    const [usersCount, setUsersCount] = useState([]);
+
+    const [productsCount, setProductsCount] = useState([]);
+
+    const [categoriesCount, setCategoriesCount] = useState([]);
+    
+
+    useEffect(function (){
+        (async ()=> {
+        apiUsers = await fetch("http://localhost:3000/api/users")
+        console.log(apiUsers)
+        apiUsers = await apiUsers.json();
+        console.log(apiUsers.count);
+        setUsersCount(apiUsers.count)
+
+        apiProducts = await fetch("http://localhost:3000/api/products")
+        console.log(apiProducts)
+        apiProducts = await apiProducts.json();
+        console.log(apiProducts.count);
+        setProductsCount(apiProducts.count)
+
+        setCategoriesCount(Object.keys(apiProducts.countByGenre).length)
+
+    })()
+    }, []) 
+
+    usersInDb.cuantity = usersCount;
+    productsInDb.cuantity = productsCount;
+    categoriesInDb.cuantity = categoriesCount;
+
     return (
     
         <div className="row">
@@ -47,4 +88,8 @@ function ContentRowMovies(){
     )
 }
 
+
+
 export default ContentRowMovies;
+
+
