5b86ab75f89ff0fefbd406f089011445a04c98bb
bottassiJuanIgnacio <bottassi2012@gmail.com>
2022-02-02 23:11:42 -0300

    Arreglo warning chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,21 +1,21 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import ChartRow from './ChartRow';
 
 
 
-function Chart (){
+function Chart() {
     const [productList, setProductList] = useState([]);
     const [page, setPage] = useState(0);
-    
+
 
     const indieWave = 'http://localhost:3000'
 
-    useEffect( function () {
-      (async () => {
-        let apiProducts = await fetch(`${indieWave}/api/products?page=${page}`)
-        apiProducts = await apiProducts.json();
-        setProductList(apiProducts.data)
-      })()
+    useEffect(function () {
+        (async () => {
+            let apiProducts = await fetch(`${indieWave}/api/products?page=${page}`)
+            apiProducts = await apiProducts.json();
+            setProductList(apiProducts.data)
+        })()
     }, [page])
 
     const previousPage = () => {
@@ -24,9 +24,9 @@ function Chart (){
         }
     }
     const nextPage = () => {
-        if(page >= 4 ){
+        if (page >= 4) {
             setPage(page);
-        }else{
+        } else {
             setPage(page + 1);
         }
     }
@@ -39,29 +39,22 @@ function Chart (){
                         <thead>
                             <tr>
                                 <th>ID</th>
-                                <th>Nombre</th>
-                                <th>Descripción</th>
+                                <th>Name</th>
+                                <th>Description</th>
                             </tr>
                         </thead>
                         <tbody>
                             {
-                            productList.map( ( row , i) => {
-                                return <ChartRow { ...row} key={i}/>
-                            })
+                                productList.map((row, i) => {
+                                    return <ChartRow {...row} key={i} />
+                                })
                             }
                         </tbody>
-                        
+
                     </table>
-                    <tfoot>
-                        <tr>
-                            <th> 
-                                <button className='background-thistle' onClick={previousPage}>Anterior</button>
-                            </th>
-                            <th>
-                                <button className='background-thistle'onClick={nextPage}>Siguiente</button>
-                            </th>
-                        </tr>
-                    </tfoot> 
+                    <button className='background-thistle' onClick={previousPage}>Anterior</button>
+                    <button style={{ marginLeft: "20px" }} className='background-thistle' onClick={nextPage}>Siguiente</button>
+
                 </div>
             </div>
         </div>
