b6e5bbff0af3549b7fd56acef2371d07b16657e9
bottassiJuanIgnacio <bottassi2012@gmail.com>
2022-02-04 12:42:39 -0300

    correccion en chartrow

--- src/components/ChartRow.js
+++ src/components/ChartRow.js
@@ -3,14 +3,13 @@ import React from 'react';
 
 function ChartRow(props){
     return (
-            
-                <tr>
-                    <td>{props.name}</td>
-                    <td>{props.id}</td>
-                    <td>{props.description}</td>
-                </tr>
-            )
-    }
+        <tr>
+            <td>{props.id}</td>
+            <td>{props.name}</td>
+            <td>{props.description}</td>
+        </tr>
+    )
+}
     
         
 export default ChartRow;
